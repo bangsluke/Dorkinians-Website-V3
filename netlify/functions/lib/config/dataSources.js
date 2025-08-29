a72001441042977c733f439feee04b38bad9eb68
Luke Bangs <bangsluke@gmail.com>
2025-08-29 10:09:06 +0100

    Synced the data sources and moved all data into the database-dorkinians repo

--- netlify/functions/lib/config/dataSources.js
+++ netlify/functions/lib/config/dataSources.js
@@ -1,3 +1,28 @@
+/**
+ * Data Sources Configuration for Dorkinians Website (Netlify Functions)
+ * 
+ * ⚠️  DO NOT MODIFY THIS FILE DIRECTLY ⚠️
+ * This file is automatically copied from database-dorkinians/config/dataSources.js
+ * 
+ * To update data sources:
+ * 1. Edit the file in database-dorkinians/config/dataSources.js
+ * 2. Run: npm run sync-config
+ * 3. This file will be automatically updated
+ * 
+ * Auto-synced on 2025-08-29T09:08:34.533Z
+ */
+/**
+ * Master
+ * Data Sources Configuration for Dorkinians Database
+ * 
+ * This file defines:
+ * 1. CSV data source URLs for Google Sheets
+ * 2. Data source types and metadata
+ * 3. Helper functions for filtering and querying
+ * 
+ * TEST: Git hook should now work with batch file (enhanced debugging)
+ */
+
 const dataSources = [
 	// Stats Data (Google Sheets CSVs)
 	{
@@ -50,6 +75,39 @@ const dataSources = [
 		url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSTuGFCG-p_UAnaoatD7rVjSBLPEEXGYawgsAcDZCJgCSPyNvqEgSG-8wRX7bnqZm4YtI0TGiUjdL9a/pub?gid=14183891&single=true&output=csv",
 		type: "StatsData",
 	},
+	// FA Site Data (The FA Full Time)
+	{
+		name: "FA_2016-17_1stXI_Results",
+		url: "https://fulltime.thefa.com/results.html?selectedSeason=559712266&selectedFixtureGroupKey=&selectedRelatedFixtureOption=2&selectedClub=938244043&selectedTeam=506738919&selectedDateCode=all&previousSelectedFixtureGroupAgeGroup=&previousSelectedFixtureGroupKey=&previousSelectedClub=938244043",
+		type: "FASiteData",
+		season: "2016-17",
+		team: "1st XI",
+		category: "results"
+	},
+	{
+		name: "FA_2016-17_1stXI_Table",
+		url: "https://fulltime.thefa.com/table.html?selectedSeason=559712266&selectedDivision=8368223&ftsTablePageContent.fixtureAnalysisForm.standingsTableDay=15&ftsTablePageContent.fixtureAnalysisForm.standingsTableMonth=7&ftsTablePageContent.fixtureAnalysisForm.standingsTableYear=2025&activeTab=1",
+		type: "FASiteData",
+		season: "2016-17",
+		team: "1st XI",
+		category: "table"
+	},
+	{
+		name: "FA_2016-17_2ndXI_Results",
+		url: "TBC",
+		type: "FASiteData",
+		season: "2016-17",
+		team: "2nd XI",
+		category: "results"
+	},
+	{
+		name: "FA_2016-17_2ndXI_Table",
+		url: "TBC",
+		type: "FASiteData",
+		season: "2016-17",
+		team: "2nd XI",
+		category: "table"
+	}
 ];
 
 const getDataSourcesByType = (type) => {
