327c8428424067e02eee99dda6dceb25712edb64
Luke Bangs <bangsluke@gmail.com>
2025-09-05 15:45:03 +0100

    Updated the schema again after removing the git hook

--- netlify/functions/lib/config/dataSources.js
+++ netlify/functions/lib/config/dataSources.js
@@ -9,7 +9,7 @@
  * 2. Run: npm run sync-config
  * 3. This file will be automatically updated
  * 
- * Auto-synced on 2025-09-05T14:40:54.228Z
+ * Auto-synced on 2025-09-05T14:44:43.672Z
  */
 /**
  * Master
