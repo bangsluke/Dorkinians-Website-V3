be06873695e24b8a2b596542e5626709bfee99b2
Luke Bangs <bangsluke@gmail.com>
2025-09-22 07:35:45 +0100

    Fix for player loading on production site

--- config/config.ts
+++ config/config.ts
@@ -1,5 +1,5 @@
 export const appConfig = {
-	version: "1.1.7",
+	version: "1.1.8",
 	name: "Dorkinians FC",
 	description: "Comprehensive source for club statistics, player performance, and team insights",
 	author: "Luke Bangs",
