676df425d375939c937a9e366f3a0c6442309adf
Luke Bangs <bangsluke@gmail.com>
2025-11-22 22:17:46 +0000

    Minor styling fixes

--- components/stats/PlayerStats.tsx
+++ components/stats/PlayerStats.tsx
@@ -538,7 +538,7 @@ function FantasyPointsSection({
 					/>
 				</div>
 				{/* Dark grey box with player name and score */}
-				<div className='bg-gray-800 rounded-lg px-3 py-2 md:px-4 md:py-3 text-center min-w-[100px] md:min-w-[120px]'>
+				<div className='bg-black rounded-lg px-3 py-2 md:px-4 md:py-3 text-center min-w-[100px] md:min-w-[120px]'>
 					<p className='text-white text-xs md:text-sm font-medium mb-1'>{playerName}</p>
 					<p className='text-2xl md:text-3xl font-bold text-white leading-none'>
 						{Math.round(fantasyBreakdown.totalFantasyPoints || 0)}
