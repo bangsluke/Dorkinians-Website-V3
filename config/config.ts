1a0a18098975e99cef7c7bde57011531b7a766c9
Luke Bangs <bangsluke@gmail.com>
2026-02-22 09:07:24 +0000

    Added missed version bump

--- config/config.ts
+++ config/config.ts
@@ -1,5 +1,5 @@
 export const appConfig = {
-	version: "3.0.4",
+	version: "3.1.0",
 	name: "Dorkinians FC",
 	description: "Comprehensive source for club statistics, player performance, and team insights",
 	author: "Luke Bangs",
